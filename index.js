bb783d35cfa267c305a8377fced20aea9e4c00ac
cfghjnhbgyt <gs1jaykat99@hotmail.com>
2024-05-09 13:13:09 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) return "";
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    let j = 0;
-    while (
-      j < prefix.length &&
-      j < strs[i].length &&
-      prefix.charAt(j) === strs[i].charAt(j)
-    ) {
-      j++;
-    }
-    prefix = prefix.substring(0, j);
-  }
-  return prefix;
+function greet(name) {
+  console.log("Hello, " + name + "!");
 }
+
+greet("Bob");
