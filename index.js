eff97b1d93eb54117c75b79c5e0fb5722d794f92
cfghjnhbgyt <gs1jaykat99@hotmail.com>
2024-05-12 10:15:12 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-      else left = mid + 1;
-    } else {
-      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-      else right = mid - 1;
-    }
-  }
-  return -1;
-}
+const numbers = [1, 5, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); // 8
